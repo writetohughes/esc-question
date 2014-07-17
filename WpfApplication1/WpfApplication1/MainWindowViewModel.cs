3f6124a42e435e8732f1195a6160db124f9ee916
IamJonez <bbrinkmann@escautomation.com>
2014-07-17 09:07:40 -0600

    Error Handler in WPF with Data Binding

---
+++ WpfApplication1/WpfApplication1/MainWindowViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication1
+{
+    class MainWindowViewModel : IDataErrorInfo
+    {
+        public MainWindowViewModel()
+        {}
+        public string ValidateInputText
+        { get; set; }
+
+        public ICommand ValidateInputCommand
+        {
+            get { return new WpfApplication1.RelayCommand(); }
+            set { }
+        }
+
+        private int age = 20;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string this[string columnName]
+        {
+            get 
+            {
+                if ("ValidateInputText" == columnName)
+                {
+                    if (String.IsNullOrEmpty(ValidateInputText))
+                    {
+                        return "Please enter a name";
+                    }
+                }
+                else if ("Age" == columnName)
+                {
+                    if (age < 0)
+                    {
+                        return "Age should be greater than zero";
+                    }
+                }
+                return "";
+            }
+        }
+    }
+
+    class RelayCommand : ICommand
+    {
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+        }
+
+
+    }
+}
